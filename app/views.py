598e92825e9c2f38df6b5a3347c0dc20f30d1bac
Alexander <ram181524@gmail.com>
2024-04-05 20:55:01 +0300

    сломался хедер

--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+
+QUESTIONS = [
+    {
+        "id": i,
+        "title": f"Question {i}",
+        "text": f"This is question number {i}"
+    } for i in range(15)
+]
+
+def index(request):
+    return render(request, "index.html",{"questions": QUESTIONS})
+
+
+def hot(request):
+    return render(request, "hot.html",{"questions": QUESTIONS})
