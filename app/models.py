6c99ea277bc6292de70d24edcdab51829e02cb75
Alexander <ram181524@gmail.com>
2024-04-08 11:21:30 +0300

    updated models, views, paggination works in /hot

--- app/models.py
+++ app/models.py
@@ -1,3 +1,49 @@
 from django.db import models
 
 # Create your models here.
+USER = [
+    {
+        'id': id,
+        'avatar': f'img/avatar{id+1}.jpeg',
+        'name': f'User{id}',
+        'rating': 100 * id
+    } for id in range(33)
+]
+
+ANSWERS = [
+    {
+        'id': id,
+        'user': USER[id % 3],
+        'rating': 100 - id,
+        'text': f'Ответ на вопрос с номером -{id}',
+        'right_flag': False,
+        'time_ago': f"{id} minutes ago"
+    } for id in range(10)
+]
+
+TAGS = [
+    {
+        'id': id,
+        'name': f'Tag {id}',
+    } for id in range(12)
+]
+
+QUESTIONS = [
+    {
+        'id': id,
+        'user': USER[id],
+        'title': f'Question {id}',
+        'text': f'This is question number {id}',
+        'rating': 10 * id, 
+        "answers_amount": id + 1,
+        'answers': ANSWERS[:id + 1],
+        'tags': TAGS[id % 8:id % 8 + 3],
+        'time_ago': f'{id} minutes ago'
+    } for id in range(30)
+]
+
+
+
+
+
+
--- app/views.py
+++ app/views.py
@@ -1,29 +1,27 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.core.paginator import Paginator
+from app import models
 
 # Create your views here.
 
 
-QUESTIONS = [
-    {
-        "id": i,
-        "title": f"Question {i}",
-        "text": f"This is question number {i}"
-    } for i in range(30)
-]
+
+
 
 def index(request):
     page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 7)
+    paginator = Paginator(models.QUESTIONS, 7)
     page_obj = paginator.page(page_num)
     
     return render(request, "index.html", {"questions": page_obj, "num_pages": paginator.num_pages})
 
 def hot(request):
-    questions = QUESTIONS[5:]
-    return render(request, "hot.html", {"questions": questions})
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(models.QUESTIONS[5:], 7)
+    page_obj = paginator.page(page_num)
+    return render(request, "hot.html", {"questions": page_obj, "num_pages": paginator.num_pages})
 
 def question(request, question_id):
-    item = QUESTIONS[question_id]
+    item = models.QUESTIONS[question_id]
     return render(request, "question_detail.html", {"question": item})
